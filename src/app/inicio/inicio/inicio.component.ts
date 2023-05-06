8eaf53238a60d734075287ef0a231d452b54d28f
Emmanuel Marcos Gabriel Villalba <evillalba@frba.utn.edu.ar>
2023-05-06 01:19:49 -0300

    inicio codeo.

--- src/app/inicio/inicio/inicio.component.ts
+++ src/app/inicio/inicio/inicio.component.ts
@@ -3,17 +3,91 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
+  ngOnInit(): void {}
+  tiempo = 0;
+  tppr = 0;
+  ipr = 0;
+  i = 11; // leer desde input
+  j = 11; // leer desde input
+  n = 11; // leer desde input
+  //habitaciones: Habitacion[] = [new Habitacion(numeroHabitacion=1), 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  disponibilidad = true;
+  camasLibres = 0;
+  precioHabitacion = 10;
+  precioTotal = 0;
+  fechaReserva = 0;
+  // oc: any[][] = [
+  //   [this.habitaciones],
+  //   [this.camasLibres],
+  //   [this.fechaReserva],
+  // ];
+  cantidadPersonas = 0;
+  tiempoEstadia: number = 0;
+  //tiempoAnticipadoReserva: number=0;
 
-  constructor() { }
+  // class Habitacion {
+  // numeroHabitacion: number=0;
+  // camas: Camas[];
+  // }
 
-  cantidadCamas(){
-    return 5;
-  }
+  //  class Cama{
+  //   numeroCama=0;
+  //   ocupada =false;
+  // }
 
-  ngOnInit(): void {
-  }
+  // generarCantidadPersonas() {
+  //   return 1;
+  // }
+  // generarTiempoEstadia() {
+  //   return 1;
+  // }
+  // generarTiempoAnticipacionReserva() {
+  //   return 1;
+  // }
 
-}
+  // precio(precio: number, cantidadPersonas: number, tiempoEstadia: number) {
+  //   return precio + cantidadPersonas * this.precioHabitacion * tiempoEstadia;
+  // }
+
+  // tiempoProximoPedidoReserva(tiempo: number, ipr: number) {
+  //   return tiempo + ipr;
+  // }
+
+  // calcularFechaReserva(tiempo: number, tar: number) {
+  //   return tiempo + tar;
+  // }
+
+  // constructor() {}
+
+  // generarIntervaloPedidosReserva() {
+  //   //intervalo entre pedidos
+  //   return 1;
+  // }
+
+  // simular() {
+  //   this.tiempo = this.tppr;
+  //   this.tppr = this.tiempoProximoPedidoReserva(this.tiempo, this.generarIntervaloPedidosReserva());
+
+  //   this.cantidadPersonas = this.generarCantidadPersonas();
+  //   this.tiempoEstadia = this.generarTiempoEstadia();
+  //   this.fechaReserva = this.calcularFechaReserva(
+  //     this.tiempo,
+  //     this.generarTiempoAnticipacionReserva()
+  //   );
+
+  // for(let i=0; i<this.i; i++){
+  //   for(let k=0; k <this.tiempoEstadia && this.disponibilidad; k++){
+  //     for(let j=0; j<this.n; j++){
+
+  //       }
+  //     }
+  //   }
+  // }
+
+  // }
+
+}  
+
