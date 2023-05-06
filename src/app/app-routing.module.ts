557832be92ae432ed3930ba5cbb0c9db3c9c1d26
manurocck <manuelnielsen.5g@gmail.com>
2023-05-05 23:00:09 -0300

    Commit Inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: ()=> import('./inicio/inicio.module').then(m=>m.InicioModule) 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
